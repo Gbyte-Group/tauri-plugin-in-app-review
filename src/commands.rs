cb190bbee3720df6e2f412b762d4fd6c3b8cc20f
xly <lonyu@gbyte.com>
2026-02-25 15:14:09 +0800

    feat: release v0.2

--- src/commands.rs
+++ src/commands.rs
@@ -5,15 +5,5 @@ use crate::Result;
 
 #[command]
 pub(crate) async fn request_review<R: Runtime>(app: AppHandle<R>) -> Result<()> {
-    // println!("Rust: request_review command called");
-    #[cfg(mobile)]
-    {
-        // println!("Rust: calling mobile request_review");
-        return app.in_app_review().request_review();
-    }
-    #[cfg(not(mobile))]
-    {
-        // println!("Rust: unsupported platform");
-        return Err(crate::Error::UnsupportedPlatform);
-    }
+    return app.in_app_review().request_review();
 }
